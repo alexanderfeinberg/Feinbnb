11394c83e7a81032eb2e3b42c85e2374b0b0d131
alexander1fein <feinberg.alexander@gmail.com>
2022-09-21 19:07:28 -0400

    Phase1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRouter = require('./api')
 const router = express.Router()
 
-router.use(apiRouter)
+router.use('/api',apiRouter)
 
 
 router.get('/api/csrf/restore', (req,res)=>{
