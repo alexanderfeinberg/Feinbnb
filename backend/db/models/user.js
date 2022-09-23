2b271c019093d286437ae8e74252c095ea85529b
alexander1fein <feinberg.alexander@gmail.com>
2022-09-23 00:04:35 -0400

    Add User authentication Express middleware

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,8 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-const bcrypt = require('bcryptjs')
+"use strict";
+const { Model } = require("sequelize");
+const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -14,97 +12,97 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
 
-    static getCurrentUserById(id){
-      return User.scope("currentUser").findByPk(id)
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
     }
 
-    static async login({credential, password}){
-      const { Op } = require('sequelize');
-      const user = await User.scope('loginUser').findOne({
-        where:{
-          [Op.or]:{
-            username:credential,
-            email:credential
-          }
-        }
-      })
+    static async login({ credential, password }) {
+      const { Op } = require("sequelize");
+      const user = await User.scope("loginUser").findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential,
+          },
+        },
+      });
 
-      if(user && user.validatePassword(password)){
-        return await User.scope('currentUser').findByPk(user.id)
+      if (user && user.validatePassword(password)) {
+        return await User.scope("currentUser").findByPk(user.id);
       }
     }
 
-    static signup({ username, email, password }){
-      const hashedPassword = bcrypt.hashSync(password)
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
         username,
         email,
-        hashedPassword
-      })
+        hashedPassword,
+      });
 
-      return await User.scope('currentUser').findByPk(user.id)
+      return await User.scope("currentUser").findByPk(user.id);
     }
 
-    toSafeObject(){
-      const { id, username, email } = this
-      return {id, username, email}
+    toSafeObject() {
+      const { id, username, email } = this;
+      return { id, username, email };
     }
 
-    validatePassword(password){
-      return bcrypt.compareSync(password, this.hashedPassword.toString())
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
     }
   }
-  User.init({
-    username: {
-      type: DataTypes.STRING,
-      allowNull:false,
-      unique:true,
-      validate:{
-        len:[4,30],
-        isNotEmail(value){
-          if(validator.isEmail(value)){
-            throw new Error("Cannot be an email.")
-          }
-        }
-      }
-
-    },
-    email: {
-      type:DataTypes.STRING,
-      allowNull:false,
-      unique:true,
-      validate:{
-        len: [3,256],
-        isEmail:true
-      }
-    },
-    hashedPassword: {
-      type:DataTypes.STRING.BINARY,
-      allowNull:false,
-      validate:{
-        len:[60,60]
-
-      }
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    defaultScope:{
-      attributes:{
-        exclude:['hashedPassword', 'email',
-        ,'createdAt','updatedAt']
-      }
-    },
-    scopes:{
-      currentUser:{
-        attributes:{
-          exclude:['hashedPassword']
-        }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          },
+        },
       },
-      loginUser:{
-        attributes:{}
-      }
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", , "createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: {
+            exclude: ["hashedPassword"],
+          },
+        },
+        loginUser: {
+          attributes: {},
+        },
+      },
     }
-  });
+  );
   return User;
 };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,28 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const { restoreUser } = require("../../utils/auth.js");
 
-router.post('/test', (req,res)=>{
-    res.json({requestBody:req.body})
-})
+router.use(restoreUser);
 
-module.exports = router
+// router.post("/test", (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
+
+// router.get("/set-token-cookie", async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: "Demo-lition",
+//     },
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
+
+// router.get("/restore-user", (req, res) => {
+//   return res.json(req.user);
+// });
+
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
+module.exports = router;
