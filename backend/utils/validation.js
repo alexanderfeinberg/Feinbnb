51b95c2c22760d0a399461daa6d791a173556397
alexander1fein <feinberg.alexander@gmail.com>
2022-09-23 10:10:12 -0400

    Adding validation error handling

---
+++ backend/utils/validation.js
@@ -0,0 +1,19 @@
+const { validationResult } = require("express-valodator");
+
+const handleValidationErrors = (req, _res, next) => {
+  const validationErrors = validationResult(req);
+
+  if (!validationErrors.isEmpty()) {
+    const errors = validationErrors.array().map((error) => `${error.msg}`);
+    const err = Error("Bad request.");
+    err.status = 400;
+    err.title = "Bad request.";
+    next(err);
+  }
+
+  next();
+};
+
+module.exports = {
+  handleValidationErrors,
+};
