b89a57e64a271032df1b23297b1e64533756aaa8
alexander1fein <feinberg.alexander@gmail.com>
2022-09-27 14:28:05 -0400

    Fixing spots

--- backend/db/seeders/20220927030240-spots.js
+++ backend/db/seeders/20220927030240-spots.js
@@ -50,7 +50,7 @@ const spots = [
     price: 150.0,
   },
   {
-    ownerId: 2,
+    ownerId: 4,
     address: "5 Chares Lane",
     city: "Miami",
     state: "Florida",
@@ -62,7 +62,7 @@ const spots = [
     price: 250.0,
   },
   {
-    ownerId: 3,
+    ownerId: 4,
     address: "6 Chares Lane",
     city: "San Francisco",
     state: "California",
@@ -74,7 +74,7 @@ const spots = [
     price: 250.0,
   },
   {
-    ownerId: 2,
+    ownerId: 5,
     address: "7 Chares Lane",
     city: "San Diego",
     state: "California",
@@ -86,7 +86,7 @@ const spots = [
     price: 220.0,
   },
   {
-    ownerId: 3,
+    ownerId: 5,
     address: "8 Chares Lane",
     city: "Washington",
     state: "District of Columbia",
