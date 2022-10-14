fe21888646b0ec05ff2e326810c0791f1497e59b
alexander1fein <feinberg.alexander@gmail.com>
2022-10-14 12:41:34 -0400

    Phase1

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -26,6 +26,15 @@ export const login = (user) => async (dispatch) => {
     }),
   });
   const data = await response.json();
+  console.log("DATA ", data);
+  dispatch(setUser(data));
+  return response;
+};
+
+export const restoreUser = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  console.log("DATA ", data);
   dispatch(setUser(data));
   return response;
 };
