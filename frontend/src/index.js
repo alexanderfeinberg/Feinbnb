3d381abdffb9acfbcf2729267b1fa6542d9432d3
alexander1fein <feinberg.alexander@gmail.com>
2022-10-13 23:28:09 -0400

    Phase1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import App from "./App";
 
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
