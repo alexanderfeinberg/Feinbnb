291b4ab72ba32a115fd9e52a2b777ea00419d70e
alexander1fein <feinberg.alexander@gmail.com>
2022-10-20 14:01:14 -0400

    Bug fix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -23,7 +23,9 @@ if (process.env.NODE_ENV === "production") {
 
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    path.resolve(__dirname, "../../frontend", "build", "index.html");
+    return res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
   });
 }
 
