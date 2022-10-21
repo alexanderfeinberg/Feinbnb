af26a4fc6dc803c0430d1285247f07884760eca1
alexander1fein <feinberg.alexander@gmail.com>
2022-10-21 10:18:57 -0400

    disabled inputs

--- frontend/src/components/Spots/SpotBookingForm.js
+++ frontend/src/components/Spots/SpotBookingForm.js
@@ -7,16 +7,26 @@ const SpotBookingForm = () => {
         <div className="booking-check-dates">
           <div className="booking-check-in">
             <label htmlFor="check-in-date">CHECK-IN</label>
-            <input type="text" name="check-in-date" value={"10/03/2022"} />
+            <input
+              type="text"
+              name="check-in-date"
+              value={"10/03/2022"}
+              disabled
+            />
           </div>
           <div className="booking-check-out">
             <label htmlFor="check-out-date">CHECK-OUT</label>
-            <input type="text" value={"10/07/2022"} name="check-out-date" />
+            <input
+              type="text"
+              value={"10/07/2022"}
+              name="check-out-date"
+              disabled
+            />
           </div>
         </div>
         <div className="booking-check-guests">
           <label htmlFor="guest-number">GUESTS</label>
-          <input type="text" value={"1 guest"} name="guest-number" />
+          <input type="text" value={"1 guest"} name="guest-number" disabled />
         </div>
       </div>
     </form>
